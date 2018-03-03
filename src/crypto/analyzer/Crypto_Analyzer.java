069cafc24672050d4c7b619c33cf3ee556a788af
NickHill1999 <nkhlshnd99@gmail.com>
2018-03-03 15:40:49 -0500

    Added Canadian Dollar Functionality

--- src/crypto/analyzer/Crypto_Analyzer.java
+++ src/crypto/analyzer/Crypto_Analyzer.java
@@ -21,7 +21,8 @@ public class Crypto_Analyzer {
      */
     public static void main(String[] args) throws IOException {
         String cryptos[] = {"bitcoin","ethereum","ripple","litecoin","dogecoin"};
-        DecimalFormat cash = new DecimalFormat("$#,###,##0.00####");
+        double crypto_val[] = new double[cryptos.length];
+        DecimalFormat cash = new DecimalFormat("$#,###,##0.00#### CAD");
         URL url = new URL("https://coinmarketcap.com/currencies/bitcoin/");
         URLConnection urlConn = url.openConnection();
         InputStreamReader inStream = new InputStreamReader(urlConn.getInputStream());
@@ -31,7 +32,7 @@ public class Crypto_Analyzer {
         int start = 0;
         int end = 0;
         
-        
+        //finding crypto values
         for(int i = 0; i < cryptos.length; i++){
             url = new URL("https://coinmarketcap.com/currencies/" + cryptos[i] +  "/");
             urlConn = url.openConnection();
@@ -45,10 +46,34 @@ public class Crypto_Analyzer {
                     price = Double.parseDouble(line.substring(start, end));
                     break;
                 }
-
                 line = buff.readLine();
             }
-            System.out.println(cryptos[i] + ": " + cash.format(price));
+            crypto_val[i] = price;
         }
+        
+        //class='uccResultAmount
+        url = new URL("http://www.xe.com/currencyconverter/convert/?Amount=1&From=USD&To=CAD");
+        urlConn = url.openConnection();
+        inStream = new InputStreamReader(urlConn.getInputStream());
+        buff = new BufferedReader(inStream);
+        String line = buff.readLine();
+        while(line!=null){
+            if(line.contains("class='uccResultAmount")){
+                end = line.indexOf("</span><span class='uccToCurrencyCode'>") - 1;
+                start = line.indexOf("<span class='uccResultAmount'>") + "<span class='uccResultAmount'>".length();
+                price = Double.parseDouble(line.substring(start, end));
+                break;
+            }
+            line = buff.readLine();
+        }
+        
+        double USD_to_CAD = price;
+        System.out.println(USD_to_CAD);
+        
+        
+        for(int i = 0; i < cryptos.length; i++){
+            System.out.println(cryptos[i] + ": " + crypto_val[i] * USD_to_CAD);
+        }
+        
     }
 }
